f938741d0923fe13d20567b1243bbe958a4acae2
Juan Lee <juanlee@kaist.ac.kr>
2018-11-02 23:28:44 +0900

    Disk

--- Back-end/Disk.py
+++ Back-end/Disk.py
@@ -5,21 +5,34 @@
 
 from pickle import dumps, loads
 
-def _format(filename):
-	return "./STORAGE/" + filename
+class Question:
+	def __init__(self, questioner = None, question = None):
+		self.questioner = questioner
+		self.question = question
+		self.answers = []
 
-def get(filename):
-	try:
-		with open(_format(filename), 'rb') as f:
-			data = f.read()
-		return data
-	except:
-		return None
+	def dump(self):
+		arg = [self.questioner, self.question, self.answers]
+		return dumps(arg)
 
-def post(filename, payload):
-	try:
-		with open(_format(filename), 'wb') as f:
-			f.wrtie(payload)
-		return data
-	except:
-		return None
+	def load(self, data):
+		arg = loads(data)
+
+		self.questioner = arg[0]
+		self.question = arg[1]
+		self.answers = arg[2]
+
+class Answer:
+	def __init__(self, answerer = None, answer = None):
+		self.answerer = answerer
+		self.answer = answer
+
+	def dump(self):
+		arg = [self.answerer, self.answer]
+		return dumps(arg)
+
+	def load(self, data):
+		arg = loads(data)
+
+		self.answerer = arg[0]
+		self.answer = arg[1]
