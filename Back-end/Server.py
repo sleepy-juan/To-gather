cbedae3d3dc3ea138db516a96392d63306fe6b21
Juan Lee <juanlee@kaist.ac.kr>
2018-11-02 22:44:28 +0900

    Server Template

--- Back-end/Server.py
+++ Back-end/Server.py
@@ -1,5 +1,31 @@
 # Server.py
-# - Main Server Program for To-gather
+# - To-gather Server Program
 #
 # Author @ Juan Lee (juanlee@kaist.ac.kr)
 
+import socket
+from System import fork, lock, wait, alarm, repeat, cancel
+
+class Server:
+	LISTENQ = 1024
+
+	def __init__(self, PORT):
+		self.sock = socket.socket()
+		self.sock.bind(('', PORT))
+		self.sock.listen(Server.LISTENQ)
+		self.clients = {}
+
+		def accept_handler(argument):
+			sock, clients, handler = argument
+			while True:
+				client, address = sock.accept()
+				username = client.recv(64).strip().decode()
+				clients[username] = client
+
+				fork(handler, (client, username))
+
+		fork(accept_handler, (self.sock, self.clients, self.per_clients))
+
+	def per_clients(self, sock, username):
+		while True:
+			pass
