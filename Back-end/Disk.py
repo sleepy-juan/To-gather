b86b1f78b665a0eb56ff215cecf20c55938317a5
Juan Lee <juanlee@kaist.ac.kr>
2018-11-03 00:18:26 +0900

    Disk

--- Back-end/Disk.py
+++ Back-end/Disk.py
@@ -3,16 +3,138 @@
 #
 # Author @ Juan Lee (juanlee@kaist.ac.kr)
 
-from pickle import dumps, loads
+from pickle import dumps, loads, dump, load
+from access_points import get_scanner
 
+# class Database
+# - database module
+class Database:
+	DB_PATH = "./Storage/"
+	DB_PATH_HELP = DB_PATH + "Help"
+	DB_PATH_USER = DB_PATH + "User"
+
+	@staticmethod
+	def logHelp(user_from, user_to):
+		try:
+			with open(DB_PATH_HELP, 'rb') as f:
+				data = load(f)
+		except:
+			data = []
+
+		data.append((user_from, user_to))
+		with open(DB_PATH_HELP, 'wb') as f:
+			dump(data, f)
+
+	@staticmethod
+	def howManyHelp(user_from, user_to):
+		try:
+			with open(DB_PATH_HELP, 'rb') as f:
+				data = load(f)
+		except:
+			data = []
+
+		n = 0
+		for uf, ut in data:
+			if uf == user_from and ut == user_to:
+				n += 1
+		return n
+
+	@staticmethod
+	def logUser(user):
+		try:
+			with open(DB_PATH_USER, 'rb') as f:
+				data = load(f)
+		except:
+			data = []
+
+		data.append(user)
+		with open(DB_PATH_USER, 'wb') as f:
+			dump(data, f)
+
+	@staticmethod
+	def getUser(username):
+		try:
+			with open(DB_PATH_USER, 'rb') as f:
+				data = load(f)
+		except:
+			data = []
+
+		query = list(filter(lambda user: user.username == username))
+		if len(query) == 0:
+			return None
+		return query[0]
+
+	@staticmethod
+	def logQuestion(question):
+		try:
+			with open(DB_PATH_QUESTION, 'rb') as f:
+				data = load(f)
+		except:
+			data = []
+
+		data.append(question)
+		with open(DB_PATH_QUESTION, 'wb') as f:
+			dump(data, f)
+
+	@staticmethod
+	def getQuestions(question_id):
+		try:
+			with open(DB_PATH_QUESTION, 'rb') as f:
+				data = load(f)
+		except:
+			data = []
+
+		return data
+
+# class User
+# - contain user information
+class User:
+	_ID_SERIAL = 0
+
+	def __init__(self, username = None):
+		if username != None:
+			self.username = username
+
+			scanner = get_scanner()
+			aps = scanner.get_access_points()
+			self.bssid = list(map(lambda x:x.bssid, aps))
+			self.id = User._ID_SERIAL
+			User._ID_SERIAL += 1
+		else:
+			self.username = None
+			self.bssid = []
+			self.id = -1
+
+	def dump(self):
+		arg = [self.username, self.bssid, self.id]
+		return dumps(arg)
+
+	def load(self, data):
+		arg = loads(data)
+
+		self.username = arg[0]
+		self.bssid = arg[1]
+		self.id = arg[2]
+
+# class Question
+# - contain question information
+# - with answers related.
 class Question:
+	_ID_SERIAL = 0
+
 	def __init__(self, questioner = None, question = None):
 		self.questioner = questioner
 		self.question = question
 		self.answers = []
 
+		if questioner != None and question != None:
+			self.id = Question._ID_SERIAL
+			Question._ID_SERIAL += 1
+		else:
+			self.id = -1
+
 	def dump(self):
-		arg = [self.questioner, self.question, self.answers]
+		arg = [self.questioner, self.question, self.answers, self.id]
 		return dumps(arg)
 
 	def load(self, data):
@@ -21,14 +143,25 @@ class Question:
 		self.questioner = arg[0]
 		self.question = arg[1]
 		self.answers = arg[2]
+		self.id = arg[3]
 
+# class Answer
+# - contain ansewr information
 class Answer:
+	_ID_SERIAL = 0
+
 	def __init__(self, answerer = None, answer = None):
 		self.answerer = answerer
 		self.answer = answer
 
+		if answerer != None and answer != None:
+			self.id = Answer._ID_SERIAL
+			Answer._ID_SERIAL += 1
+		else:
+			self.id = -1
+
 	def dump(self):
-		arg = [self.answerer, self.answer]
+		arg = [self.answerer, self.answer, self.id]
 		return dumps(arg)
 
 	def load(self, data):
@@ -36,17 +169,4 @@ class Answer:
 
 		self.answerer = arg[0]
 		self.answer = arg[1]
-
-data = b'asdfdsafadsf'
-
-q = Question()
-q.load(data)
-
-q.question = ?
-q.questioner = ?
-q.answers = [Answer]
-
-a = Answer()
-
-a.answerer = ?
-a.answer = ?
+		self.id = arg[2]
