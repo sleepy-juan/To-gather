b9b2fe5f0722ff2b1ccbea997d088d429f4667f3
marunero <j0070ak@kaist.ac.kr>
2018-11-19 00:37:20 +0900

    fix commompoint

--- Back-end/Common.py
+++ Back-end/Common.py
@@ -8,11 +8,11 @@ CLUB = ['ART', 'MUSIC', 'EXERCISE']
 
 def common():
 	user_info = {}
-	user_info['school'] = SCHOOL[random.randrange(0, len(SCHOOL))]
-	user_info['grade'] = GRADE[random.randrange(0, len(GRADE))]
-	user_info['major'] = MAJOR[random.randrange(0, len(MAJOR))]
-	user_info['location'] = LOCATION[random.randrange(0, len(LOCATION))]
-	user_info['club'] = CLUB[random.randrange(0, len(CLUB))]
+	user_info['we are in same school'] = SCHOOL[random.randrange(0, len(SCHOOL))]
+	user_info['we are in same grade'] = GRADE[random.randrange(0, len(GRADE))]
+	user_info['we have same major'] = MAJOR[random.randrange(0, len(MAJOR))]
+	user_info['we are in same location'] = LOCATION[random.randrange(0, len(LOCATION))]
+	user_info['we have same club'] = CLUB[random.randrange(0, len(CLUB))]
 	return user_info
 
 def user_common(map, userA, userB):
@@ -20,10 +20,3 @@ def user_common(map, userA, userB):
 		if map[userA][i] == map[userB][i]:
 			return i
 	return 'none'
-
-answerer = 
-questioner = 
-if Database.howManyHelp() == 0:
-	question.content_common = user_common(user_info, answerer, questioner)
-else:
-	question.content_common = 'help' + str(Database.howManyHelp())
